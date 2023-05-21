c29089364e39452a8f65dc503adf1e20002c0a59
vamsi876 <vamsikrishnakollipara1234@gmail.com>
2023-05-21 18:52:47 +0530

    WIP: Learningapp

---
+++ learningapp/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div className='heading'>
+        <h1>Header</h1>
+        <p style ={{color:"red"}}>Contact Us</p>
+    </div>
+  )
+}
---
+++ learningapp/src/components/Hello.js
@@ -0,0 +1,5 @@
+function Hello(){
+    return(<>
+    Hello worls!</>)
+}
+export default Hello
